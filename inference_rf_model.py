c043a0c038e464cce1e758ba54e09f3b1855b884
jinbeom <ppgt1225@naver.com>
2024-10-31 01:56:25 +0900

    fix inference model

--- inference_rf_model.py
+++ inference_rf_model.py
@@ -1,15 +1,33 @@
-import joblib, time
+import joblib, struct
 import numpy as np
 
-from tqdm.auto import tqdm
+from utils.udp_lib import UdpServer
 
 
 est_model = joblib.load('rf_model.joblib')
-time_list = []
+udp_handle = UdpServer(address='localhost', port=6341)
 
-for i in tqdm(range(1000)):
-    t0 = time.time()
-    pred_output = est_model.predict(np.random.rand(1, 15).astype(np.float32))
-    time_list.append(time.time() - t0)
+while True:
+    output_msg = bytes(0)
+    read_msg = udp_handle.receive_msg()
 
-print(np.mean(time_list))
+    boom_angle = struct.unpack('f', read_msg[0:4])[0]
+    swing_angle = struct.unpack('f', read_msg[4:8])[0]
+    load_weight = struct.unpack('f', read_msg[8:12])[0]
+    roll_angle = struct.unpack('f', read_msg[12:16])[0]
+    pitch_angle = struct.unpack('f', read_msg[16:20])[0]
+
+    #  boom+roll/load, boom+pitch/load, 'wing/load, roll/pitch,  load/roll, load/pitch, boom+roll, boom+pitch, boom_x_pos, boom_y_pos
+    input_data = np.array([boom_angle, swing_angle, load_weight, roll_angle, pitch_angle,
+                           (boom_angle+roll_angle)/load_weight, (boom_angle+pitch_angle)/load_weight, swing_angle/load_weight,
+                           roll_angle/pitch_angle, load_weight/roll_angle, load_weight/pitch_angle, boom_angle+roll_angle,
+                           boom_angle+pitch_angle, np.cos(np.deg2rad(swing_angle)) * np.cos(np.deg2rad(boom_angle)),
+                           np.sin(np.deg2rad(swing_angle)) * np.cos(np.deg2rad(boom_angle))])
+
+    pred_output_arr = est_model.predict(input_data.reshape(1, -1))[0]
+
+    for i, pred_output in enumerate(pred_output_arr):
+        output_msg += struct.pack('f', pred_output)
+
+    udp_handle.send_msg(output_msg)
+
