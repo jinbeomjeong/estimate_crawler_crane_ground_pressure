2067b13a98ca27b0bfcd3ea93731fb5ff5fc7781
jinbeom, jeong <ppgt1225@naver.com>
2024-10-31 19:10:49 +0900

    add monitoring app

--- inference_rf_model.py
+++ inference_rf_model.py
@@ -2,32 +2,29 @@ import joblib, struct
 import numpy as np
 
 from utils.udp_lib import UdpServer
+from utils.Msg_Parser import crane_state_msg_parse, crane_under_load_parse
 
 
 est_model = joblib.load('rf_model.joblib')
 udp_handle = UdpServer(address='localhost', port=6341)
 
 while True:
-    output_msg = bytes(0)
     read_msg = udp_handle.receive_msg()
+    crane_state_arr = crane_state_msg_parse(read_msg)
+    boom_angle, swing_angle, load_weight, roll_angle, pitch_angle = (crane_state_arr[0], crane_state_arr[1],
+                                                                     crane_state_arr[2], crane_state_arr[3], crane_state_arr[4])
 
-    boom_angle = struct.unpack('f', read_msg[0:4])[0]
-    swing_angle = struct.unpack('f', read_msg[4:8])[0]
-    load_weight = struct.unpack('f', read_msg[8:12])[0]
-    roll_angle = struct.unpack('f', read_msg[12:16])[0]
-    pitch_angle = struct.unpack('f', read_msg[16:20])[0]
-
-    #  boom+roll/load, boom+pitch/load, 'wing/load, roll/pitch,  load/roll, load/pitch, boom+roll, boom+pitch, boom_x_pos, boom_y_pos
-    input_data = np.array([boom_angle, swing_angle, load_weight, roll_angle, pitch_angle,
-                           (boom_angle+roll_angle)/load_weight, (boom_angle+pitch_angle)/load_weight, swing_angle/load_weight,
-                           roll_angle/pitch_angle, load_weight/roll_angle, load_weight/pitch_angle, boom_angle+roll_angle,
-                           boom_angle+pitch_angle, np.cos(np.deg2rad(swing_angle)) * np.cos(np.deg2rad(boom_angle)),
-                           np.sin(np.deg2rad(swing_angle)) * np.cos(np.deg2rad(boom_angle))])
+    if load_weight != 0 and roll_angle != 0 and pitch_angle != 0:
+        #  boom+roll/load, boom+pitch/load, 'wing/load, roll/pitch,  load/roll, load/pitch, boom+roll, boom+pitch, boom_x_pos, boom_y_pos
+        input_data = np.array([boom_angle, swing_angle, load_weight, roll_angle, pitch_angle,
+                               (boom_angle+roll_angle)/load_weight, (boom_angle+pitch_angle)/load_weight, swing_angle/load_weight,
+                               roll_angle/pitch_angle, load_weight/roll_angle, load_weight/pitch_angle, boom_angle+roll_angle,
+                               boom_angle+pitch_angle, np.cos(np.deg2rad(swing_angle)) * np.cos(np.deg2rad(boom_angle)),
+                               np.sin(np.deg2rad(swing_angle)) * np.cos(np.deg2rad(boom_angle))])
+    else:
+        input_data = np.array([1]*15)
 
     pred_output_arr = est_model.predict(input_data.reshape(1, -1))[0]
-
-    for i, pred_output in enumerate(pred_output_arr):
-        output_msg += struct.pack('f', pred_output)
-
+    output_msg = crane_under_load_parse(pred_output_arr)
     udp_handle.send_msg(output_msg)
 
---
+++ utils/Msg_Parser.py
@@ -0,0 +1,20 @@
+import struct
+import numpy as np
+
+def crane_state_msg_parse(msg: bytearray) -> np.array:
+    boom_angle = struct.unpack('f', msg[0:4])[0]
+    swing_angle = struct.unpack('f', msg[4:8])[0]
+    load_weight = struct.unpack('f', msg[8:12])[0]
+    roll_angle = struct.unpack('f', msg[12:16])[0]
+    pitch_angle = struct.unpack('f', msg[16:20])[0]
+
+    return np.array([boom_angle, swing_angle, load_weight, roll_angle, pitch_angle])
+
+
+def crane_under_load_parse(load: np.array) -> bytearray:
+    output_msg = bytes(0)
+
+    for i, pred_output in enumerate(load):
+        output_msg += struct.pack('f', pred_output)
+
+    return output_msg
