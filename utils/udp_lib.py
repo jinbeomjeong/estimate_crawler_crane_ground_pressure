5926ac93f141048ff4438709365c6d9c4599e06e
jinbeom, jeong <ppgt1225@naver.com>
2024-10-31 19:46:30 +0900

    fix udp com

--- utils/udp_lib.py
+++ utils/udp_lib.py
@@ -2,11 +2,13 @@ import socket
 
 
 class UdpServer:
-    def __init__(self, address='localhost', port=6340):
+    def __init__(self, server_address='localhost', server_port=6340):
         self.server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.local_addr = address
-        self.port = port
-        self.server_socket.bind((self.local_addr, self.port))
+        self.server_address = server_address
+        self.server_port = server_port
+        self.client_address = None
+
+        self.server_socket.bind((self.server_address, self.server_port))
         print("UDP Server is running!")
 
     def disconnect(self):
@@ -14,17 +16,19 @@ class UdpServer:
         print("Disconnection Successful")
 
     def receive_msg(self):
-        return self.server_socket.recvfrom(1024)[0]
+        msg, self.client_address = self.server_socket.recvfrom(1024)
+        
+        return msg
 
-    def send_msg(self, message, address='localhost', port=6340):
-        self.server_socket.sendto(message, (address, port))
+    def send_msg(self, message, client_address='localhost', port=6340):
+        self.server_socket.sendto(message, self.client_address)
 
 
 class UdpClient:
-    def __init__(self, address='localhost', port=6340):
+    def __init__(self, server_address='localhost', server_port=6340):
         self.client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        self.addr = address
-        self.port = port
+        self.addr = server_address
+        self.port = server_port
         self.client_socket.connect((self.addr, self.port))
         print("UDP Client is Running!")
 
@@ -32,8 +36,8 @@ class UdpClient:
         self.client_socket.close()
         print("Disconnection Successful")
 
-    def send_msg(self, message, address='localhost', port=6340):
-        self.client_socket.sendto(message, (address, port))
+    def send_msg(self, message):
+        self.client_socket.sendto(message, (self.addr, self.port))
 
     def receive_msg(self):
-        return self.client_socket.recvfrom(1024)[1]
+        return self.client_socket.recvfrom(1024)[0]
